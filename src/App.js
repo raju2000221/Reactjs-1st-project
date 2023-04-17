783f58df66f3c42680103aa04d470732464a66f9
Raju Islam <bdrajuislam246@gmail.com>
2023-04-18 02:35:41 +0600

    dynamicly show value

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <Product name="Laptop" price="300"></Product>
+     <Product name="Phone" price="5000"></Product>
+     <Product name="Chosma" price="200"></Product>
     </div>
   );
 }
 
-
+function Product(props){
+  return(
+    <div className="Product">
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
+    </div>
+  )
+}
 
 export default App;
