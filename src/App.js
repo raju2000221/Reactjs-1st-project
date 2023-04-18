465675e72f53f443d72a5c6c779c0187f8a97ace
Raju Islam <bdrajuislam246@gmail.com>
2023-04-18 21:06:35 +0600

    Dynamicly add data from arry

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Prioducts = [
+    {name:'Laptop', Price:'60000', Brand:'Lenovo'},
+    {name:'Phone', Price:'100000', Brand:'Apple'},
+    {name:'Watch', Price:'2000', Brand:'Navie Force'},
+    {name:'Bike', Price:'60000', Brand:' Honda Repsol'},
+    {name:'Bike', Price:'60000', Brand:' Honda Repsol'},
+    {name:'Tv', Price:'35000', Brand:' Samsung'},
+    
+  ]
   return (
     <div className="App">
-     <Product name="Laptop" price="300"></Product>
-     <Product name="Phone" price="5000"></Product>
-     <Product name="Chosma" price="200"></Product>
+      {
+        Prioducts.map(Produc => <Product Name={Produc.name} Price={Produc.Price} B={Produc.Brand}></Product>)
+       
+      }
+     console.log(Prioducts.name)
     </div>
   );
 }
@@ -14,8 +25,9 @@ function App() {
 function Product(props){
   return(
     <div className="Product">
-      <h3>Name: {props.name}</h3>
-      <p>Price: {props.price}</p>
+      <h3>Name: {props.Name}</h3>
+      <p>Price: {props.Price} </p>
+      <p>Brand: {props.B}</p>
     </div>
   )
 }
